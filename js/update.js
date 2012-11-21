600e8d0bb94632ee351f2368ee1a346dd9b9f8b9
Ali Abdulla <ali.abdulla3@gmail.com>
2012-11-21 03:53:22 +0300

    nothing majore

--- js/update.js
+++ js/update.js
@@ -1,5 +1,6 @@
 // JavaScript Document
  
+ //Variables: =============================================== --- VARIABLES--- =======================================
 var counter=0;
 var appointed_devs_id=[];
 var appointed_devs_names=[];
@@ -10,6 +11,82 @@ to_do_id = $form.find( 'input[name="to_do_id"]' ).val();
 
 load_updates();
 hide_element("#div_update_tags",1);
+hide_element("#div_status_change",1);
+
+
+
+//Events: =============================================== --- EVENTS--- =======================================
+
+
+ $("#form1").submit(function(event) {
+	
+	// alert('FUCK OFF!, IM NOT YOUR SLAVE!');
+	 //prevent devault action of button
+	 event.preventDefault();
+	 
+	   var $form = $( this ),
+		//var details = details textarea
+        details = $form.find( 'textarea[name="details"]' ).val(),
+		//this is a hidden input	
+		to_do_id = $form.find( 'input[name="to_do_id"]' ).val(),
+		//ussrl for ajax function,, 
+        url =  "functions/update/insert_update.php";
+    	 /* Send the data using post and put the results in a div */
+   
+	  $.post( url, { appointed_devs_id:appointed_devs_id,details: details,to_do_id: to_do_id},
+	        function( data ) {
+         // var content = $( data ).find( '#content' );
+          //$( "#result" ).empty().append( content );
+		   
+		  $('#displaydata').html(data);
+		  
+      		}//end of function
+    	);//end of post
+	 
+	 //refresh list of updates
+
+	 load_updates();
+	 //reset all values
+
+
+  $('#details').val("");
+ 
+ $('#display_tagged').html(" ");
+ 
+ //delete all elements of array
+  var aLen = ( appointed_devs_id.length - 1 ); 
+   for ( var x = aLen; appointed_devs_id[ x ]; x-- ) { // Removing all 500 items from the bigArray.
+      appointed_devs_id.pop( x ); 
+	  appointed_devs_names.pop( x ); 
+	  
+   }//end for
+ 
+	
+	});//end of form submit
+
+
+//draw the status bullets
+$('#enabled_status').click(function(e){
+	 show_element("#div_status_change",350);
+	 
+	 	$.post('functions/update/change_status.php',{ },function(data){
+					//start of function
+					
+			 
+				 
+					$('#div_status_change').html(data);	
+			
+						
+						
+							
+				   }//end of function
+				);//end of $.post
+				
+			 //========================
+	 
+	
+	
+});
 
 $('#div_update_tags').live('keypress', function(e) {
 	$('#details').text(e.keyCode);
@@ -59,7 +136,13 @@ $(function() {
 	});
 });
 
+//functions: =============================================== --- FUNCTIONS--- =======================================
 
+//change the current status of to_do_update
+function change_status(element_id, value)
+{
+	hide_element('#'+element_id);
+}//end of function change)status
 function add_to_dev_array(element,dev_id,dev_name)
 {
 	
@@ -97,51 +180,7 @@ function add_to_dev_array(element,dev_id,dev_name)
 
 
 
- $("#form1").submit(function(event) {
-	
-	// alert('FUCK OFF!, IM NOT YOUR SLAVE!');
-	 //prevent devault action of button
-	 event.preventDefault();
-	 
-	   var $form = $( this ),
-		//var details = details textarea
-        details = $form.find( 'textarea[name="details"]' ).val(),
-		//this is a hidden input	
-		to_do_id = $form.find( 'input[name="to_do_id"]' ).val(),
-		//ussrl for ajax function,, 
-        url =  "functions/update/insert_update.php";
-    	 /* Send the data using post and put the results in a div */
-   
-	  $.post( url, { appointed_devs_id:appointed_devs_id,details: details,to_do_id: to_do_id},
-	        function( data ) {
-         // var content = $( data ).find( '#content' );
-          //$( "#result" ).empty().append( content );
-		   
-		  $('#displaydata').html(data);
-		  
-      		}//end of function
-    	);//end of post
-	 
-	 //refresh list of updates
 
-	 load_updates();
-	 //reset all values
-
-
-  $('#details').val("");
- 
- $('#display_tagged').html(" ");
- 
- //delete all elements of array
-  var aLen = ( appointed_devs_id.length - 1 ); 
-   for ( var x = aLen; appointed_devs_id[ x ]; x-- ) { // Removing all 500 items from the bigArray.
-      appointed_devs_id.pop( x ); 
-	  appointed_devs_names.pop( x ); 
-	  
-   }//end for
- 
-	
-	});//end of click
 
 
 function load_updates(){
