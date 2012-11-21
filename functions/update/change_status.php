83705f342f8481b53ec3e7af02e2f7df27748cb8
Ali Abdulla <ali.abdulla3@gmail.com>
2012-11-21 03:49:25 +0300

    function pages for status change button, also ignoring items/

---
+++ functions/update/change_status.php
@@ -0,0 +1,3 @@
+<li style="background-color:#FF9900" id="li_changeto_active" onClick="change_status('li_changeto_active',1 )">Active</li>
+<li style="background-color:#000" id="li_changeto_completed" onClick="change_status('li_changeto_completed',2 )">Completed</li>
+<li style="background-color:#CCC" id="li_changeto_cancelled" onClick="change_status('li_changeto_cancelled',3 )">Cancelled</li>
